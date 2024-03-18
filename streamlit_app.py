68719338ea9557f37f960c77196a2492d92beb0a
Stryes <162982974+Stryes@users.noreply.github.com>
2024-03-18 12:12:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st 
 import pandas as pd
+import numpy as np
 voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRmqinK_tqx6_ILzYYKrlGZY3oN3cRThvBCkJlLadYCXnHIIO6sQh-eulslnyXqG0JRDSUxglK6guZe/pub?output=csv')
 st.dataframe(voc)
+l=voc.shape[o]
+i= np.random:choice(range(l))
+word_fr=voc['Definiton'].values[i]
+st.write(world_fr+"  "+word_chi)
