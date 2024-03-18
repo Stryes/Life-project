1cab1bbbd352f00b48a342c1a31d127162523319
Stryes <162982974+Stryes@users.noreply.github.com>
2024-03-18 11:52:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st 
 import pandas as pd
-voc=pd.read_csv('lien')
+voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRmqinK_tqx6_ILzYYKrlGZY3oN3cRThvBCkJlLadYCXnHIIO6sQh-eulslnyXqG0JRDSUxglK6guZe/pub?output=csv')
 st.dataframe(voc)
