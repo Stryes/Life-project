8ec036b7513d09bd44a5a2f0cc5cceaa7212e744
Stryes <162982974+Stryes@users.noreply.github.com>
2024-03-18 11:39:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,4 @@
 import streamlit as st 
-input="lapin"
-list_possibilies=["rabbit","raco","vorpal","bun"]
-correct_answer="rabbit"
-st.write("comment dit on ? "+ input+ "en anglais")
-for i in range(len(list_possibilities)):
-  st.but (list_possibilities[i])
+import pandas as pd
+voc=pd.read_csv('lien')
+st.dataframe(voc)
